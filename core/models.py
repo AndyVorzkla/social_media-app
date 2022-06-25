0a834bb407481bbf72ba13a74a64434bab118a39
AndyVorzkla <andreyshadrin007@gmail.com>
2022-06-25 15:31:25 +0300

    changeProfileModel, login, signup

--- core/models.py
+++ core/models.py
@@ -1,21 +1,34 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
 
 class UUIDMixin(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
     class Meta:
         abstract = True
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    class RelationshipChoices(models.TextChoices):
+        NONE = '', _('None')
+        SINGLE = 'single', _('Single')
+        IN_A_RELATIONSHIP = 'relationship', _('In a relationship')
+        MARRIED = 'married', _('Married')
+        ENGAGED = 'engaged', _('Engaged')
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
     profile_img = models.ImageField(upload_to='profile_images/', default='default-profile.png')
     location = models.CharField(max_length=100, blank=True)
+    working_at = models.CharField(max_length=200, blank=True, default='')
+    relationship = models.TextField(_('relationship'), choices=RelationshipChoices.choices, default='', blank=True)
 
     def __str__(self):
         return self.user.username
-
--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
+
+@login_required(login_url='core:signin')
 def index(request):
     return render(request, 'core/index.html')
 
@@ -23,11 +27,47 @@ def signup(request):
             else:
                 user = User.objects.create_user(username=username,
                                                 email=email,
-                                                password=password,
-                                                password2=password2)
+                                                password=password, )
                 user.save()
+
+                # Log user in and redirect to settings page
+                user = auth.authenticate(username=username, password=password)
+                auth.login(request, user)
+                # Create a profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('core:signup')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('core:signup')
     else:
         return render(request, 'core/signup.html')
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('core:index')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('core:signin')
+    else:
+        return render(request, 'core/signin.html')
+
+
+@login_required(login_url='core:signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('core:signin')
+
+
+@login_required(login_url='core:signin')
+def settings(request):
+
+    return render(request, 'core/setting.html')
